b84c505799d3bd5d126d2ab18fe06b290ae8b24b
StSv <staslive2014@gmail.com>
2023-11-21 19:56:56 +0300

    v1.3 Дополение условия, к первому заданию и добовление задачи в отдельную ветку

--- src/main/java/org/example/Task1.java
+++ src/main/java/org/example/Task1.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Task1 {
     // Задание №1 найти средний элемент в LinkedList за один проход?
+
+
+    public static void decision(){
+        //создам элемент листа LinkedList так как я не знаю что должно быть в списке будет строки
+        ArrayList<String> LinkedList = new ArrayList<String>();
+        LinkedList.add("Один");
+        LinkedList.add("Два");
+        LinkedList.add("Три");
+
+
+        System.out.println(LinkedList);
+
+
+
+
+
+    }
 
 
 
