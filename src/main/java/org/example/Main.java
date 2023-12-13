069403c4398e2c36222c42fd5499e8ea2a4d3237
StSv <staslive2014@gmail.com>
2023-12-13 16:55:37 +0300

    v1.6 решение задачи №2 добавление условий и.т.д черновой вариант

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Task1.decision();// Запуск задание №1
+        //Task1.decision();// Запуск задание №1
+
+
+        Task2.twoSum(new int[]{2, 7, 11, 15}, 9);// Запуск задание №2
     }
 }
--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -3,4 +3,46 @@ package org.example;
 public class Task2 {
     // dddeeeklln -> 3d3e1k2l1n
     //одну строку перевести в формат
-}
+    /*
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+
+    Дан массив целых чисел nums и целое число target.
+    Вернуть индексы двух чисел в массиве, сумма которых равна target.
+    Можно предположить, что каждый входной массив имеет ровно одно решение,
+    и нельзя использовать один и тот же элемент дважды.
+    Ответ можно вернуть в любом порядке.
+
+
+
+     */
+//    int[] nums = [2,7,11,15];
+//    int target = 9;
+
+    public static int[] twoSum(int[] nums, int target) {
+
+            int n = nums.length;
+            for (int i = 0; i < nums.length - 1; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        System.out.println("найдено  "+ i +" "+ j);
+                        return new int[]{i, j};
+                    }
+                }
+            }
+            System.out.println("не найдено");
+            return new int[]{}; // не найдено
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
