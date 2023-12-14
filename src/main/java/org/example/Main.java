913f592b120b1a68b745cd93de907ffc6764b3f6
StSv <staslive2014@gmail.com>
2023-12-14 13:21:40 +0300

    v1.8 решение задачи №2 добавление ещё одного решение

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,18 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
         //Task1.decision();// Запуск задание №1
 
 
-        Task2.twoSum(new int[]{2, 7, 11, 15}, 9);// Запуск задание №2
+        //Task2.twoSumDecision(new int[]{2, 7, 11, 15, 8}, 10);// Запуск задание №2
+        Task2.twoSumDecision2(new int[]{3, 2, 7, 11, 15, 8}, 10);// Запуск задание №2
     }
+
+
+
+
 }
--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Task2 {
     // dddeeeklln -> 3d3e1k2l1n
@@ -16,13 +18,11 @@ public class Task2 {
     и нельзя использовать один и тот же элемент дважды.
     Ответ можно вернуть в любом порядке.
 
-
-
      */
 //    int[] nums = {2,7,11,15};
 //    int target = 9;
 
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSumDecision(int[] nums, int target) {
             int n = nums.length;
             for (int i = 0; i < nums.length - 1; i++) {
                 for (int j = i + 1; j < nums.length; j++) {
@@ -35,7 +35,48 @@ public class Task2 {
             System.out.println("не найдено");
             return new int[]{}; // не найдено
         }
+
+
+
+    public static int[] twoSumDecision2(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        int n = nums.length;
+
+        // строим Хэш таблицу
+        for (int i = 0; i < nums.length; i++) {
+            numMap.put(nums[i], i);
+        }
+
+        // Находим компонент
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement) && numMap.get(complement) != i) {
+                System.out.println("найдены элементы это  "+ i +"  и  "+ numMap.get(complement) +"  из массива   "+ Arrays.toString(nums));
+                return new int[]{i, numMap.get(complement)};
+            }
+        }
+        System.out.println("не найдено");
+        return new int[]{};
     }
+
+
+        public int[] twoSumDecision3(int[] nums, int target) {
+            Map<Integer, Integer> numMap = new HashMap<>();
+            int n = nums.length;
+
+            for (int i = 0; i < n; i++) {
+                int complement = target - nums[i];
+                if (numMap.containsKey(complement)) {
+                    System.out.println("найдены элементы это  "+ i +"  и  "+ numMap.get(complement) +"  из массива   "+ Arrays.toString(nums));
+                    return new int[]{numMap.get(complement), i};
+                }
+                numMap.put(nums[i], i);
+            }
+            System.out.println("не найдено");
+            return new int[]{};
+        }
+
+}
 
 
 
