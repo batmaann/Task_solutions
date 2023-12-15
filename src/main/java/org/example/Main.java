89fbaac755b6bd2225e0f467e4105367bb0f919c
StSv <staslive2014@gmail.com>
2023-12-15 16:13:41 +0300

    v1.11 Задача №4 добавление решений

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,7 +12,8 @@ public class Main {
         //Task2.twoSumDecision2(new int[]{3, 2, 7, 11, 15, 8}, 10);// Запуск задание №2
         //Task2.twoSumDecision3(new int[]{3, 2, 7, 11, 15, 8}, 10);// Запуск задание №2
         //Task3.maxProfitDecision1(new int[]{7,1,5,3,6,4});
-        Task3.maxProfitDecision2(new int[]{1,2,3,4,5,6});
+        //Task3.maxProfitDecision2(new int[]{1,2,3,4,5,6});
+        Task4.containsDuplicateDecision1(new int[]{1,2,3,4,5,6,8,7});
 
     }
 
