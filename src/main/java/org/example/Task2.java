9831b306866a19f6d9107e5c05549fa5cd3b315b
StSv <staslive2014@gmail.com>
2023-12-20 15:18:56 +0300

    v1.12 Задача №2 убрал лишний коментарий

--- src/main/java/org/example/Task2.java
+++ src/main/java/org/example/Task2.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Task2 {
-    // dddeeeklln -> 3d3e1k2l1n
+
     //одну строку перевести в формат
     /*
     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
