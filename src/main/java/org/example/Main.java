0fa44a16d656482457c2a29d06f76ddfca36095a
StSv <staslive2014@gmail.com>
2024-02-12 12:27:42 +0300

    v1.17 исправление ошибок в описании файла readme.md

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Task1.decision();// Запуск задание №1
+
+
+        //Task2.twoSumDecision(new int[]{2, 7, 11, 15, 8}, 10);// Запуск задание №2
+        //Task2.twoSumDecision2(new int[]{3, 2, 7, 11, 15, 8}, 10);// Запуск задание №2
+        //Task2.twoSumDecision3(new int[]{3, 2, 7, 11, 15, 8}, 10);// Запуск задание №2
+        //Task3.maxProfitDecision1(new int[]{7,1,5,3,6,4});
+        //Task3.maxProfitDecision2(new int[]{2,3,4});
+        //Task4.containsDuplicateDecision1(new int[]{1,2,3,4,5,6,8,7,1});
+        //Task5.thirdMaxDecision1(new int[]{3, 2, 7, 11, 15, 8});
+        //Task6.Decision1("dddeeeklln");
+
     }
+
+
+
+
 }
